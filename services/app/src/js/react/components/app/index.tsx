51778a0c0c9d97f1fa3e0aa9adc84340203cc5f7
WebGL3D <WebGL3D@users.noreply.github.com>
2023-03-12 09:17:42 -0700

    Add scss + mui
    These are great frameworks.

--- services/app/src/js/react/components/app/index.tsx
+++ services/app/src/js/react/components/app/index.tsx
@@ -1,5 +1,19 @@
 import { Fragment } from 'react';
+import { CssBaseline, useMediaQuery } from '@mui/material';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 export default function App() {
-  return <Fragment>Hello, world!</Fragment>;
+  const useLightTheme = useMediaQuery('(prefers-color-scheme: light)');
+  const theme = createTheme({
+    palette: {
+      mode: useLightTheme ? 'light' : 'dark',
+    },
+  });
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Fragment>Hello, world!</Fragment>
+    </ThemeProvider>
+  );
 }
