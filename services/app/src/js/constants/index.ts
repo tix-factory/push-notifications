296dbcbe8b058944bd3ba3403501dab89218f562
WebGL3D <WebGL3D@users.noreply.github.com>
2023-03-12 14:30:43 -0700

    Cache bust service worker

--- services/app/src/js/constants/index.ts
+++ services/app/src/js/constants/index.ts
@@ -1,5 +1,5 @@
 // The URL where the service worker exists.
-const serviceWorkerUrl = './service-worker.js';
+const serviceWorkerUrl = './service-worker.js?cache-buster=1';
 
 export { serviceWorkerUrl };
 export { default as pushPublicKey } from './pushPublicKey';
