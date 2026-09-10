2d9abdb6f645aef688c60d98bfecf47647c004d1
Bob Rose <WebGL3D@users.noreply.github.com>
2026-09-10 12:57:22 -0400

    Add wrangler.jsonc

--- services/app/vite.config.ts
+++ services/app/vite.config.ts
@@ -3,6 +3,21 @@ import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    rolldownOptions: {
+      input: {
+        index: './index.html',
+        'service-worker': './service-worker.ts',
+      },
+      output: {
+        entryFileNames: (chunkInfo) => {
+          return chunkInfo.name === 'service-worker'
+            ? 'service-worker.js'
+            : 'assets/[name]-[hash].js';
+        },
+      },
+    },
+  },
   resolve: {
     // Because we have a file dependency in the package.json, we need to deduplicate react from that depenedency.
     dedupe: ['react'],
