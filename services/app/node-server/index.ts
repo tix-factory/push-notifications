e694045db73f95b8b54980a619b669240cc1dc71
Bob Rose <WebGL3D@users.noreply.github.com>
2026-09-10 14:42:08 -0400

    Revert "no-op change"
    This reverts commit c5b524273a59ebf1cfe784a0b764ad3ca55ad7f3.

--- services/app/node-server/index.ts
+++ services/app/node-server/index.ts
@@ -26,7 +26,7 @@ export default {
     if (url.pathname.startsWith('/api/')) {
       return new Response(
         JSON.stringify({
-          error: 'Unimplemented',
+          error: 'unimplemented',
         }),
         {
           headers: { 'Content-Type': 'application/json' },
