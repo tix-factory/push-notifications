c65d28733d72f5da38ca0c339e8e2c05e59e3c5e
Bob Rose <WebGL3D@users.noreply.github.com>
2026-09-17 09:11:46 -0400

    Add Rust register endpoint (#30)

--- services/app/secrets.sh
+++ services/app/secrets.sh
@@ -2,6 +2,7 @@
 
 # Generate the keys
 openssl genpkey -out auth_private_key.pem -algorithm RSA -pkeyopt rsa_keygen_bits:2048
+openssl rsa -in auth_private_key.pem -pubout -out auth_public_key.pem
 openssl ecparam -genkey -name prime256v1 | openssl ec -out vapid_keypair.pem
 openssl ec -in vapid_keypair.pem -pubout -out public_key.pem
 openssl ec -in vapid_keypair.pem -out private_key.pem
--- services/app/src/controllers/vapid.rs
+++ services/app/src/controllers/vapid.rs
@@ -1,16 +1,16 @@
-use std::env;
-use std::sync::LazyLock;
 use axum::{
     body::Body,
-    extract::State,
     http::{Response, StatusCode},
     response::IntoResponse,
     Json
 };
 use axum_extra::extract::{cookie::Cookie, CookieJar};
-use serde::Serialize;
+use chrono::{Utc};
+use serde::{Serialize, Deserialize};
 use base64::{engine::general_purpose::URL_SAFE_NO_PAD, Engine};
-use crate::utils::{VAPID_PUBLIC_KEY, EMAIL_ADDRESS};
+use jsonwebtoken::{encode, decode, Header, Extras, Algorithm, Validation, EncodingKey, DecodingKey};
+use crate::utils::{VAPID_PUBLIC_KEY, JWT_PRIVATE_KEY};
+
 
 const AUTH_COOKIE_NAME: &str = "auth_token";
 
@@ -19,6 +19,40 @@ struct Metadata {
     /// The URL-safe base64 encoded VAPID public key.
     #[serde(rename = "publicKey")]
     public_key: String
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct RegisterPayload {
+    /// The endpoint to send the notification to.
+    endpoint: String,
+
+    /// When the push subscription will expire.
+    ///
+    /// Browsers have the ability to expire the endpoint after a set amount of time.
+    /// This property reflects the expiration, if it exists.
+    /// See also: https://developer.mozilla.org/en-US/docs/Web/API/PushSubscription/expirationTime
+    expiration: Option<String>,
+
+    /// The (base64 encoded) public key the push subscription is subscribed with.
+    p256dh: String,
+
+    /// The (base64 encoded) authentication secret for the push subscription.
+    auth: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct AuthCookie {
+    /// The endpoint to send the notification to.
+    sub: String,
+
+    /// The (base64 encoded) public key the push subscription is subscribed with.
+    p256dh: String,
+
+    /// The (base64 encoded) authentication secret for the push subscription.
+    auth: String,
+
+    /// The cookie expiration.
+    exp: usize,
 }
 
 /// The metadata endpoint used by the web app to load the VAPID public key.
@@ -33,12 +67,29 @@ pub async fn metadata() -> Response<Body> {
 }
 
 /// Forgets the push notification registration information.
-pub async fn unregister(cookies: CookieJar) -> StatusCode {
+pub async fn register(cookies: CookieJar, Json(request): Json<RegisterPayload>) -> (StatusCode, CookieJar) {
+    let claims = AuthCookie {
+        sub: request.endpoint,
+        p256dh: request.p256dh,
+        auth: request.auth,
+        exp: (Utc::now().timestamp() + (3600 * 24 * 7)) as usize
+    };
+    let token = encode(&Header::new(Algorithm::RS256), &claims, &JWT_PRIVATE_KEY).expect("Failed to encode JWT");
+    let cookie = Cookie::build((AUTH_COOKIE_NAME, token))
+        .path("/")
+        .max_age(time::Duration::days(7))
+        .build();
+
+    (StatusCode::NO_CONTENT, cookies.add(cookie))
+}
+
+
+/// Forgets the push notification registration information.
+pub async fn unregister(cookies: CookieJar) -> (StatusCode, CookieJar) {
     let cookie = Cookie::build((AUTH_COOKIE_NAME, ""))
         .path("/")
         .max_age(time::Duration::ZERO)
         .build();
-    let _ = cookies.remove(cookie);
 
-    StatusCode::NO_CONTENT
+    (StatusCode::NO_CONTENT, cookies.remove(cookie))
 }
