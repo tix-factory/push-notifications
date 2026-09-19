56e9a146b7b84a6be9072a33c47dc89a09bbbae4
Bob Rose <WebGL3D@users.noreply.github.com>
2026-09-19 09:57:30 -0400

    Move JWT key reads to env.rs
    Consistency

--- services/app/src/env.rs
+++ services/app/src/env.rs
@@ -3,6 +3,14 @@ use p256::PublicKey;
 use std::env;
 use std::sync::LazyLock;
 
+/// Reads the `JWT__PUBLIC_KEY` from the environment variables.
+pub static JWT_PUBLIC_KEY: LazyLock<String> =
+    LazyLock::new(|| env::var("JWT__PUBLIC_KEY").expect("JWT__PUBLIC_KEY is not set."));
+
+/// Reads the `JWT__PRIVATE_KEY` from the environment variables.
+pub static JWT_PRIVATE_KEY: LazyLock<String> =
+    LazyLock::new(|| env::var("JWT__PRIVATE_KEY").expect("JWT__PRIVATE_KEY is not set."));
+
 /// Reads the `VAPID__EMAIL_ADDRESS` from the environment variables.
 pub static EMAIL_ADDRESS: LazyLock<String> =
     LazyLock::new(|| env::var("VAPID__EMAIL_ADDRESS").expect("VAPID__EMAIL_ADDRESS is not set."));
@@ -13,6 +21,6 @@ pub static VAPID_PUBLIC_KEY: LazyLock<PublicKey> = LazyLock::new(|| {
     PublicKey::from_public_key_pem(&raw_public_key).expect("VAPID__PUBLIC_KEY (PEM) is not set.")
 });
 
-/// Reads the `VAPID_PRIVATE_KEY` PEM from the environment variables.
+/// Reads the `VAPID__PRIVATE_KEY` PEM from the environment variables.
 pub static VAPID_PRIVATE_KEY: LazyLock<String> =
     LazyLock::new(|| env::var("VAPID__PRIVATE_KEY").expect("VAPID__PRIVATE_KEY is not set."));
